4aa49b0faa7f1633a1158ee8a030c1a0fe9d6ffd
4z0t <gastec139@gmail.com>
2023-05-22 23:07:45 +0300

    add GridData

---
+++ GameOfLife/GridData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    internal class GridData : IData<CellState>
+
+    {
+        public CellState this[int x, int y]
+        {
+            get => CellState.Alive;
+            set
+            {
+
+            }
+        }
+
+    }
+}
